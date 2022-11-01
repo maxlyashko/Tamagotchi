b5c771fb837d1f60f455c160471a7278e69d0a44
molodoysever <92895042+molodoysever@users.noreply.github.com>
2022-11-01 23:07:29 +0200

    Delete src/main/java/com/lyashko/hw7 directory

--- src/main/java/com/lyashko/hw7/Characters.java
+++
@@ -1,8 +0,0 @@
-package com.lyashko.hw7;
-
-public enum Characters {
-    Deer,
-    Lion,
-    Cat,
-    Dog;
-}
--- src/main/java/com/lyashko/hw7/Main.java
+++
@@ -1,7 +0,0 @@
-package com.lyashko.hw7;
-
-public class Main {
-    public static void main ( String[] args ) {
-        GameUtil.Runner ();
-    }
-}
